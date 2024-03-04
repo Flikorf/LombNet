ea1f83950bdcfd8466fff6b80a947402ff54dbf3
MorozovTimofey <104879885+MorozovTimofey@users.noreply.github.com>
2024-03-04 16:25:27 +0500

    New home window design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,19 @@
+import styles from "../Home/Home.module.css";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Главная страница</h1>
-      <Link to="/login">
-        <button>Войти</button>
-      </Link>
-    </div>
+    <>
+      <header className={styles.header}>
+        <Link to="/">
+          <img src="logo.svg" alt="" />
+        </Link>
+        <Link to="/login">
+          <button className={styles.login}>Войти</button>
+        </Link>
+      </header>
+      <hr />
+    </>
   );
 };
 
